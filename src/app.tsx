3a38e41898baa137ad391c216ebb4ccf00e91237
eddie-luong <hongluong296@gmail.com>
2025-03-13 04:36:44 +0000

    init base project

---
+++ src/app.tsx
@@ -0,0 +1,27 @@
+import { Button } from '@/components/ui/button'
+import '@/global.css'
+import viteLogo from '/vite.svg'
+
+export function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <Button>My Button</Button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
